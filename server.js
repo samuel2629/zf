01dacf095b065904e5561ab4299c5f0c20ff7700
Samuel <samuel2629@gmail.com>
2017-10-22 14:21:17 +0200

    a

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('./index.html');
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(./index.html);
+  res.end(index);
 }).listen(process.env.PORT || 5000);
 console.log('Server currently listening...');
