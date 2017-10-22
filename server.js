0232b4ca0d42c1245d63e19aea8301a89f7ce9ba
Samuel <samuel2629@gmail.com>
2017-10-22 14:36:04 +0200

    s

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ fs.readFileSync('index.html', function (err, html){
 
   if(err) throw err;
   http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'utf-8'});
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(html);
     res.end();
   }).listen(process.env.PORT || 5000);
